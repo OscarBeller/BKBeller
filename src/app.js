dc0faa8fb1e7ca880a381bcc38c48c0cbfdf23ac
Oscar Beller <gabrieloscarbeller@hotmail.com>
2024-04-16 22:42:32 -0300

    Cambios realizado

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import express from "express";
+import { Server } from "socket.io";
+import { engine } from "express-handlebars";
 import { router as productRouter } from "./routes/productRouter.js";
 import { router as cartRouter } from "./routes/cartRouter.js";
+import __dirname from "./utils.js"
+
 const PORT = 3000;
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+// Handlebars setup
+app.engine('handlebars', engine());
+app.set('views', __dirname + './views');
+app.set('view engine', 'handlebars');
+
+app.get('/', (req,res)=>{
+    return res.render('home')
+})
 
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
